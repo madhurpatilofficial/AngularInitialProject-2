0f8c7abe78423bd40cb860d0749d42910118df4a
Madhur Dhondu Patil <madhurpatilofficial@gmail.com>
2023-11-30 12:16:32 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as prod from '../Model/Products.json'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ProjectAngular';
+ 
+  products : any = (prod as any).default;
+  constructor(){
+    console.log(prod);
+  }
 }
